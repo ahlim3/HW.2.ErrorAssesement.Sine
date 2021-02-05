83ed925ecae98313ee15f8d4af4ddfae3876c943
Anthony Lim <anthony.hong.cheol.lim@gmail.com>
2021-02-05 11:40:08 -0600

    Initial Commit

---
+++ Shared/HW_2_ErrorAssesementApp.swift
@@ -0,0 +1,17 @@
+//
+//  HW_2_ErrorAssesementApp.swift
+//  Shared
+//
+//  Created by Anthony Lim on 2/5/21.
+//
+
+import SwiftUI
+
+@main
+struct HW_2_ErrorAssesementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
